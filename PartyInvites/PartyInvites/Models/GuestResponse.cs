07a5538012fa4456869645db9dcaa96a461b3aa0
farmijo <farmijo16@gmail.com>
2018-09-03 17:21:27 +0200

    first commit

---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please, enter a value!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please, enter a value!")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please, enter a value!")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please, enter a value!")]
+        public bool? WillAttend { get; set; }
+        
+    }
+}
