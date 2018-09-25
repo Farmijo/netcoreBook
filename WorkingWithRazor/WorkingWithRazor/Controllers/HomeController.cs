25e4a26b058195cacd8dc1f3080809f3d6e26fbc
Fran <farmijo16@gmail.com>
2018-09-25 15:57:40 +0200

    WorkingWithRazors Chapter

---
+++ WorkingWithRazor/WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkingWithRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public ViewResult Index()
+        {
+
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+            return View(myProduct);
+        }
+    }
+}
