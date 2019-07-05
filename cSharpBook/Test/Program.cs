b41cf19ff8bf2bfde37e21df29714f736ad9e188
Fran <farmijo16@gmail.com>
2019-07-05 09:59:06 +0200

    Simple webapi

---
+++ cSharpBook/Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var nuevoStruct = new TestStruct();
+
+            nuevoStruct.cadenaTexto ="Hola";
+            nuevoStruct.numero = 1;
+
+            Console.WriteLine($"{nuevoStruct.numero}");
+
+        }
+    }
+}
---
+++ cSharpBook/Test/TestClass.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Test{
+
+    struct TestStruct{
+
+        public int numero;
+        public string cadenaTexto;
+
+    }
+
+    class TestClass{
+
+        public int numeroReemplazable;
+        
+
+    }
+}
